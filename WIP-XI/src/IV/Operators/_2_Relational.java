9eab65fca7a1a7dfa88ee8fe4fc7e862c84525f3
benjamaporn <janplang5@gmail.com>
2019-05-31 08:43:49 +0700

    Add project

---
+++ WIP-XI/src/IV/Operators/_2_Relational.java
@@ -0,0 +1,27 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package IV.Operators;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class _2_Relational {
+    public static void main(String[] args) {
+        int x = 66;
+        
+        System.out.println("== : " + (x == 11));
+        
+        System.out.println("!= : " + (x != 10));
+        
+        System.out.println("> : " + (x > 7));
+        
+        System.out.println("< : " + (x < 0));
+        
+        System.out.println(">= : " + (x >= 9));
+        
+        System.out.println("<= : " + (x <= 4));
+    }
+}
